0c275ebb09a0232757e018e20784570065f6b084
Brian Lambert <brianlambert2408@gmail.com>
2022-11-01 13:55:38 +1100

    Completed tasks

--- methods.rb
+++ methods.rb
@@ -1,1 +1,21 @@
 # Your code here!
+def greet_programmer
+    print "Hello, programmer!"
+end
+
+def greet name
+    print "Hello, #{name}!"
+end
+
+def greet_with_default(name="programmer")
+    print "Hello, #{name}!"
+end
+
+def add num1, num2
+    num1 + num2
+end
+
+def halve num
+    num / 2  if num.class == Integer
+end
+
